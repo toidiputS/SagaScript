3380d583e1b050f14956cdd2d5012e2a0779fa5b
one2onemill <40640432-one2onemill@users.noreply.replit.com>
2025-03-20 10:18:21 +0000

    Restored to 'a3664a0a3a7d147598a5337f041684b870d7681e'
    Replit-Restored-To: a3664a0a3a7d147598a5337f041684b870d7681e

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-ureimport { Switch, Route, useLocation } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
